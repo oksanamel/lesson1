7b3f237ac710bd6f61f79a59b31c37143f5b50de
oksanamel <157284043+oksanamel@users.noreply.github.com>
2024-02-14 11:04:48 +0300

    Lesson4 add Task1

---
+++ Lesson4/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿//int[,] matrix = new int [3, 5];
+//Random rnd = new Random(); 
+//for (int i = 0; i < matrix.GetLength(0); i++)
+//{
+    
+//for (int j = 0; j < matrix.GetLength(1); j++) 
+//{
+  //   matrix[i, j] = rnd.Next(1, 11);
+//}   
+//}
+//for (int i = 0; i < matrix.GetLength(1); i++)
+//{
+    //for (int j = 0; j < matrix.GetLength(0); j++)
+   // {
+     // Console.Write($"{matrix[i, j]} ");  
+    //}
+    //Console.WriteLine();
+//}
+
+int [,] CreateMatrix(int rownCount, int columnsCount)
+{
+    int[,] matrix = new int[rownCount, columnsCount];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix; 
+}
+void ShowMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        System.Console.WriteLine();
+} 
+}
+int[,] matrix = CreateMatrix(4, 5);
+ShowMatrix(matrix);
